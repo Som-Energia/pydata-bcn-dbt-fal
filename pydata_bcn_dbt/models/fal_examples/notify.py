4811fe1f5e55ecdfe4065d2affacf8c33fdc260a
Diego Quintana <daquintanav@gmail.com>
2023-04-27 19:20:20 +0000

    add notify script

--- pydata_bcn_dbt/models/fal_examples/notify.py
+++ pydata_bcn_dbt/models/fal_examples/notify.py
@@ -1,17 +1,11 @@
 from decouple import config
 from novu.api import EventApi
 
-event_api = EventApi("https://novu-api:3000/api/", config("NOVU_API_KEY"))
+event_api = EventApi("http://novu-api:3000", config("NOVU_API_KEY"))
+
 
 event_api.trigger(
     name="on-boarding-notification",
     recipients="6446cda01e1018fd0f5af967",
-    payload={"message": "Welcome to Novu!"},
-)
-
-
-event_api.trigger(
-    name="on-boarding-notification",
-    recipients="on-boarding-subscriber-id-123",
-    payload={"message": "Welcome to Novu!"},
+    payload={"message": "Hello, pydata!"},
 )
